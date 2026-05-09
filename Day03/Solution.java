303ea32b8299b7a19b29a8c434a8c24c314c27c7
Adarshanshu <adarshmpvm@gmail.com>
2026-05-09 12:19:28 +0530

    updated the binarySearch code for all possibilities

--- Day03/Solution.java
+++ Day03/Solution.java
@@ -2,14 +2,23 @@ package DSA_Practise.Day03;
 
 import java.util.*;
 public class Solution {
-    public static void binarySearch(int nums[],int target){
+    public static int binarySearch(int nums[],int target){
         int start =0;
         int end = nums.length-1;
+        if(nums.length==0){
+            return 0;
+        }
+        if(nums.length==1){
+            if(nums[0]==target){
+                return 0;
+            }else{
+                return -1;
+            }
+        }
         while(start<=end){
             int mid = (start+end)/2;
             if(nums[mid]==target){
-                System.out.println("the index of trget is "+mid);
-                break;
+                return mid;
             }
             else if(target>nums[mid]){
                 start = mid+1;
@@ -18,6 +27,7 @@ public class Solution {
                 end = mid-1;
             }
         }
+        return -1;
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
@@ -31,7 +41,13 @@ public class Solution {
         }
          System.out.println("enter the target you want to find ");
         int target = sc.nextInt();
-        binarySearch(nums,target);
+        int result = binarySearch(nums,target);
+        if(result!=-1){
+            System.out.println("target index is at -> "+result);
+        }
+        else{
+            System.out.println("your target is not in thhe array ");
+        }
         sc.close();
     }
 }
